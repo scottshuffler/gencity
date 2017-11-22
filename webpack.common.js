ac01cb9c08f9569276894f640c6e9105b02c1be6
Scott Shuffler <scottshuffler@gmail.com>
2017-11-22 11:39:10 -0500

    dev works now

--- webpack.common.js
+++ webpack.common.js
@@ -2,24 +2,43 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
 module.exports = {
   entry: {
     app: './src/index.js',
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { 
+        test: /\.js$/,
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+        query: {
+         presets: ['react', 'es2015']
+        } 
+      },
+      { 
+        test: /\.jsx$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Production',
-    }),
+    HtmlWebpackPluginConfig,
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js',
   },
+ 
 };
--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,22 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { 
+        test: /\.js$/,
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+        query: {
+         presets: ['react', 'es2015']
+        } 
+      },
+      { 
+        test: /\.jsx$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
     ],
   },
   plugins: [HtmlWebpackPluginConfig],
